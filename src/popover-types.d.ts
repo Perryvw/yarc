612a87a667a72fff9eaef45a4cee10d303a0f6fd
Perryvw <perryvw@users.noreply.github.com>
2024-10-28 20:38:30 +0100

    Add TS types for popover API

---
+++ src/popover-types.d.ts
@@ -0,0 +1,6 @@
+// This is a (hopefully) temporary file to include types for the popover API in react
+// Seems they will be adding this in react 19, maybe a future version of the @types/react?
+// see: https://github.com/DefinitelyTyped/DefinitelyTyped/pull/69670#issuecomment-2131788974
+// and https://github.com/facebook/react/pull/27981#issuecomment-2131792986
+
+/// <reference types="react/canary" />
