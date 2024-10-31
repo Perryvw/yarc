6cfae80a4296da0b49679f3a02ee2ffc12a6780a
Pavel Djundik <xPaw@users.noreply.github.com>
2024-10-31 10:45:21 +0200

    Change request things to be dropdowns

--- src/renderer/src/RequestPanel.tsx
+++ src/renderer/src/RequestPanel.tsx
@@ -3,8 +3,7 @@ import { AppContext } from "./AppContext";
 import CodeMirror from "@uiw/react-codemirror";
 import styled from "styled-components";
 import KeyValuesPanel from "./KeyValuesPanel";
-import { Tab, Tabs } from "./Tabs";
-import classNames from "classnames";
+import { ChevronUp } from "lucide-react";
 
 const RequestPanelRoot = styled.div`
     display: flex;
@@ -15,6 +14,40 @@ const RequestPanelRoot = styled.div`
     border-right: 1px solid hsla(0, 0%, 100%, 0.075);
 `;
 
+const RequestSection = styled.details`
+    margin: 20px;
+
+    & > summary .chevron {
+        margin-left: auto;
+    }
+
+    &[open] > summary .chevron {
+        transform: rotate(180deg);
+    }
+`;
+
+const RequestSectionHeader = styled.summary`
+    display: flex;
+    align-items: center;
+    gap: 5px;
+    cursor: pointer;
+    margin-bottom: 10px;
+`;
+
+const RequestSectionHeaderName = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 5px;
+
+    &.has-dot:after {
+        background: hsl(201, 86%, 67%);
+        border-radius: 50%;
+        width: 8px;
+        height: 8px;
+        content: "";
+    }
+`;
+
 export default function RequestPanel() {
     const context = useContext(AppContext);
 
@@ -23,8 +56,6 @@ export default function RequestPanel() {
     const [requestBody, setRequestBody] = useState(
         activeRequest ? (activeRequest.type === "http" ? activeRequest.body : "protobuf") : "",
     );
-
-    const [tab, setTab] = useState<"parameters" | "body" | "headers">("parameters");
 
     context.addActiveRequestListener(RequestPanel.name, (r) => {
         if (r) {
@@ -47,67 +78,44 @@ export default function RequestPanel() {
 
     return (
         <RequestPanelRoot>
-            <Tabs>
-                <Tab
-                    type="button"
-                    className={classNames({
-                        active: tab === "parameters",
-                    })}
-                    onClick={() => setTab("parameters")}
-                >
-                    Parameters
-                </Tab>
-                <Tab
-                    type="button"
-                    className={classNames({
-                        active: tab === "body",
-                        "has-dot": requestBody?.length > 0,
-                    })}
-                    onClick={() => setTab("body")}
-                >
-                    Body
-                </Tab>
-                <Tab
-                    type="button"
-                    className={classNames({
-                        active: tab === "headers",
-                    })}
-                    onClick={() => setTab("headers")}
-                >
-                    Headers
-                </Tab>
-            </Tabs>
+            <RequestSection open>
+                <RequestSectionHeader>
+                    <RequestSectionHeaderName>Parameters</RequestSectionHeaderName>
+                    <ChevronUp size={20} className="chevron" />
+                </RequestSectionHeader>
+                <KeyValuesPanel />
+            </RequestSection>
 
-            {tab === "parameters" && (
-                <div>
-                    <KeyValuesPanel />
-                </div>
-            )}
+            <RequestSection open>
+                <RequestSectionHeader>
+                    <RequestSectionHeaderName className={requestBody?.length > 0 ? "has-dot" : ""}>
+                        Body
+                    </RequestSectionHeaderName>
+                    <ChevronUp size={20} className="chevron" />
+                </RequestSectionHeader>
+                Type:
+                <button type="button">application/json</button>
+                <button type="button">application/x-www-form-urlencoded</button>
+                <KeyValuesPanel />
+                <CodeMirror
+                    theme="dark"
+                    basicSetup={{ foldGutter: true }}
+                    style={{
+                        flexBasis: "100%",
+                        overflow: "hidden",
+                    }}
+                    value={requestBody}
+                    onChange={onRequestBodyChanged}
+                />
+            </RequestSection>
 
-            {tab === "body" && (
-                <div>
-                    Type:
-                    <button type="button">application/json</button>
-                    <button type="button">application/x-www-form-urlencoded</button>
-                    <KeyValuesPanel />
-                    <CodeMirror
-                        theme="dark"
-                        basicSetup={{ foldGutter: true }}
-                        style={{
-                            flexBasis: "100%",
-                            overflow: "hidden",
-                        }}
-                        value={requestBody}
-                        onChange={onRequestBodyChanged}
-                    />
-                </div>
-            )}
-
-            {tab === "headers" && (
-                <div>
-                    <KeyValuesPanel />
-                </div>
-            )}
+            <RequestSection open>
+                <RequestSectionHeader>
+                    <RequestSectionHeaderName>Headers</RequestSectionHeaderName>
+                    <ChevronUp size={20} className="chevron" />
+                </RequestSectionHeader>
+                <KeyValuesPanel />
+            </RequestSection>
         </RequestPanelRoot>
     );
 }
