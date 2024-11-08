0e52bb1ab04b12a1f87af0b5984345df0f3c2f00
Pavel Djundik <xPaw@users.noreply.github.com>
2024-11-08 12:10:05 +0200

    Change test server to output plain text; add `sleep` param

--- test/http-test-server.ts
+++ test/http-test-server.ts
@@ -3,14 +3,6 @@ import { parse as parseUrl } from "node:url";
 
 const PORT = process.env.PORT ?? 50080;
 
-function tryParseJSON(str: string) {
-    try {
-        return JSON.parse(str);
-    } catch {
-        return str;
-    }
-}
-
 async function collectBody(req: IncomingMessage): Promise<string> {
     const chunks: Buffer[] = [];
     for await (const chunk of req) {
@@ -28,25 +20,35 @@ const server = createServer(async (req, res) => {
 
         const body = await collectBody(req);
 
-        const requestInfo = {
-            timestamp: timestamp,
-            method: req.method ?? "UNKNOWN",
-            url: req.url ?? "",
-            httpVersion: req.httpVersion,
-            headers: req.headers,
-            queryParams: url.query,
-            pathname: url.pathname,
-            body: tryParseJSON(body),
-        };
+        if (url.query.sleep) {
+            const sleep = Number.parseInt(url.query.sleep as string);
+            await new Promise((resolve) => setTimeout(resolve, sleep));
+        }
 
         res.writeHead(200, {
-            "Content-Type": "application/json",
+            "Content-Type": "text/plain",
         });
-        res.end(JSON.stringify(requestInfo, null, 2));
+
+        res.write(`${req.method ?? "UNKNOWN"} ${req.url} HTTP/${req.httpVersion}\n`);
+
+        for (let i = 0; i < req.rawHeaders.length; i += 2) {
+            res.write(`${req.rawHeaders[i]}: ${req.rawHeaders[i + 1]}\r\n`);
+        }
+
+        res.write("\n");
+
+        if (body) {
+            res.write(body);
+        }
+
+        res.end();
     } catch (error) {
         console.error("Error processing request:", error);
-        res.writeHead(500, { "Content-Type": "application/json" });
-        res.end(JSON.stringify({ error: "Internal Server Error" }));
+        res.writeHead(500, {
+            "Content-Type": "text/plain",
+        });
+        res.write(error);
+        res.end();
     }
 });
 
